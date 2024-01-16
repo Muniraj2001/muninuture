5cad193df7b1f097ec7615c85cec25a3efd26f24
Muniraj C <munirajmechanical15@gmail.com>
2024-01-16 12:46:07 +0530

    error fixing

--- megaminds/nuturemite/nuturemite/settings.py
+++ megaminds/nuturemite/nuturemite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!xqk1&%5vi!f=cduwp%$w4l&4e04mwv_b8&@=c3jfip$+tgfmn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
